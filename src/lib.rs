047fbed5c474070d23d5d24f17efb9088eacd26f
orpheus6678 <66983935+orpheus6678@users.noreply.github.com>
2026-03-09 11:07:31 +0600

    feat: add interactivity
    specifically,
    the toggling of cell states on click
    and a play/pause button

--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,15 @@ pub enum Cell {
   Alive = 1,
 }
 
+impl Cell {
+  fn toggle(&mut self) {
+    *self = match *self {
+      Cell::Dead => Cell::Alive,
+      Cell::Alive => Cell::Dead,
+    }
+  }
+}
+
 #[wasm_bindgen]
 pub struct Universe {
   width: u32,
@@ -99,6 +108,11 @@ impl Universe {
   /// to get a reference, use [`Self::get_cells()`]
   pub fn cells(&self) -> *const Cell {
     self.cells.as_ptr()
+  }
+
+  pub fn toggle_cell(&mut self, row: u32, col: u32) {
+    let idx = self.get_index(row, col);
+    self.cells[idx].toggle();
   }
 
   pub fn tick(&mut self) {
